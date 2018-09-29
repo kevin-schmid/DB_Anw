72db4712b3f2fca9dc8f72967c3e0c54d044801c
Kevin Schmid <kevin.schmid@edu.fh-joanneum.at>
2018-09-29 12:28:40 +0200

    persistence draft

--- src/main/java/at/fhj/swd/Employee.java
+++ src/main/java/at/fhj/swd/Employee.java
@@ -1,14 +1,31 @@
 package at.fhj.swd;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Employee {
 
+    @Id
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int salary;
+
+    protected Employee(){}
+
     public Employee(int id, String firstName, String lastName, int salary) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
     }
 
     public void raiseSalary(int salaryRaise) {
+        this.salary += salaryRaise;
     }
 
-    public Object getSalary() {
-        return null;
+    public int getSalary() {
+        return salary;
     }
 }
