6d8a0aab0f09fdeaf3916bc4978b3da32dbda7d6
Kevin Schmid <kevin.schmid@edu.fh-joanneum.at>
2018-10-20 12:15:54 +0200

    create for ue4

--- ue3/src/main/resources/sql-scripts/create.sql
+++ ue3/src/main/resources/sql-scripts/create.sql
@@ -1,18 +1,19 @@
-set search_path to ue2;
+set search_path to ue4;
 
-drop sequence seq_beer;
 drop table beer;
-drop table ingredient;
+drop table recipe;
+drop sequence seq_beer;
 
 create sequence seq_beer start with 1000 increment by 1;
 
-create table ingredient (
-  ingredient_id serial primary key,
+create table recipe (
+  recipe_id serial primary key,
   name varchar(255) not null
 );
 
 create table beer (
   beer_id int primary key default nextval('seq_beer'),
   name varchar(255) not null,
-  beer_type varchar(255)
+  beer_type varchar(255),
+  recipe_id int references recipe(recipe_id) unique
 );
